986250a76c4acd82d3638d59b22869d6d559a5ce
andyjohn23 <andrewnick906@gmail.com>
2022-09-08 12:29:25 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.getElementById('searchByID');
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+            .then(response => response.json())
+            .then(json => {
+                const title = document.querySelector('#movieDetails h4');
+                const summary = document.querySelector('#movieDetails p');
+
+                title.innerText = json.title;
+                summary.innerText = json.summary;
+            });
+        input.value = "";
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
